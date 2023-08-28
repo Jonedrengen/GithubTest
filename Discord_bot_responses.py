0fd0286f5484ba319a17b893f377513430a55cc8
Jon Slotved <jonsslotved@gmail.com>
2023-08-28 12:32:12 +0200

    Simple weather report

--- Discord_bot_responses.py
+++ Discord_bot_responses.py
@@ -1,12 +1,56 @@
 import random
 import discord
 
+# Importing the required module for making HTTP requests
+import requests
+
+# Defining a function that fetches weather data based on given latitude and longitude
+def get_weather(latitude=52.52, longitude=13.41):
+    # Constructing the URL for the API request
+    url = f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m&daily=weathercode&timezone=auto&forecast_days=3'
+    
+    # Making the API request and converting the response to a JSON object
+    response = requests.get(url).json()
+    # Returning the JSON data
+    return response
+
+# Calling the function to fetch weather data and storing it in 'result'
+result = get_weather()
+
+# Defining a function to extract specific weather information from the fetched JSON data
+def get_weather_info(json_data):
+    # Getting the current temperature from the 'hourly' key of the JSON data
+    current_temperature = json_data['hourly']["temperature_2m"][0]
+    # Getting the weather code for the current day from the 'daily' key
+    weather_code_today = json_data['daily']["weathercode"][0]
+    
+    # Checking if the weather code is 3 (Assuming this indicates rain might come soon)
+    if weather_code_today == 3:
+        is_raining = "maybe soon"
+    # Checking if the weather code falls within a set of codes indicating actual rain
+    if weather_code_today in [20, 21, 22, 23, 24, 25, 26, 27, 29]:
+        is_raining = "actually raining"
+    # If none of the above conditions match, then it's not raining
+    else:
+        is_raining = "no"
+    
+    # Returning a dictionary containing the current temperature and the rain status
+    return {
+        "Temperature": current_temperature,
+        "Raining": is_raining
+    }
+
+# Calling the function to extract weather info and storing it in 'weather_info'
+weather_info = get_weather_info(result)
+
+
+
 # Handles the bot response to the users input
 
 # Basically handles how the Bot should respond. Uses 2 arguments 1 message and 1 username.
 def handle_response(message, username): # <-- When adding more arguments, remember to update all the places where this is called (send_message() function).
     p_message = message.lower()
-    if p_message in ["hello", "hey", "hi", "yo", "hej"]:
+    if p_message.lower() in ["hello", "hey", "hi", "yo", "hej"]:
         return f"what is up {username}"
     if p_message == "roll":
         return str(random.randint(1,6))
@@ -16,5 +60,8 @@ def handle_response(message, username): # <-- When adding more arguments, rememb
     help_text += "3. calc: start the calculater function of the bot."
     if p_message == "!help":
         return help_text
+    if p_message.lower() in ["weather", "forecast", "vejr", "vejret"]:
+        return weather_info
+
 
 
